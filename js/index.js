229b140aabbdab2026cea65ce37a17a241387d12
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2021-08-17 15:16:33 +0600

    js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('penguin-btn').addEventListener('click',function(){
+    const penguin = document.getElementById('penguin-text');
+    penguin.innerText = 'I AM PENGUIN';
+})
